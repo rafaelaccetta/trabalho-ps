e18af7c2615ebd9a3f6e112d37ce812e12e51da7
MiguelJehle <miguelbjh@id.uff.br>
2025-06-23 12:35:30 -0300

    CRUD de Insumos usando JPA

--- trabalho-ps/src/main/java/com/afm/trabalho_ps/model/Insumo.java
+++ trabalho-ps/src/main/java/com/afm/trabalho_ps/model/Insumo.java
@@ -25,5 +25,4 @@ public class Insumo {
         this.nome = nome;
         this.quantidade = quantidade;
     }
-
 }
--- trabalho-ps/src/main/java/com/afm/trabalho_ps/service/InsumoService.java
+++ trabalho-ps/src/main/java/com/afm/trabalho_ps/service/InsumoService.java
@@ -4,3 +4,50 @@ package com.afm.trabalho_ps.service;
 
 import com.afm.trabalho_ps.model.Insumo;
 import com.afm.trabalho_ps.repository.InsumoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class InsumoService {
+
+    @Autowired
+    private InsumoRepository insumoRepository;
+
+    // Retorna todos os insumos cadastrados
+    public List<Insumo> buscaTodos() {
+        return insumoRepository.findAll();
+    }
+
+    // Busca um insumo pelo ID
+    public Optional<Insumo> buscaPorId(Long id) {
+        return insumoRepository.findById(id);
+    }
+
+    // Salva um novo insumo
+    public Insumo cria(Insumo insumo) {
+        return insumoRepository.save(insumo);
+    }
+
+    // Atualiza um insumo existente
+    public Insumo atualiza(Long id, Insumo insumoDetails) {
+        return insumoRepository.findById(id).map(insumo -> {
+            insumo.setNome(insumoDetails.getNome());
+            insumo.setDescricao(insumoDetails.getDescricao());
+            insumo.setQuantidade(insumoDetails.getQuantidade());
+            // Adicione outros campos conforme necessário
+            return insumoRepository.save(insumo);
+        }).orElseThrow(() -> new RuntimeException("Insumo não encontrado com id " + id));
+    }
+
+    // Remove um insumo pelo ID
+    public void deleta(Long id) {
+        insumoRepository.deleteById(id);
+    }
+
+    //Remove todos insumos
+    public void deletaTodos() {
+        insumoRepository.deleteAll();
+    }
+}
