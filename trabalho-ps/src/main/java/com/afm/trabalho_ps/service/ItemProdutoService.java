3798556b11974bb2f231ffb149047ae2bb333b7b
A1lonso <alonsobbassini@gmail.com>
2025-06-23 12:42:44 -0300

    CRUD para ItemProduto

---
+++ trabalho-ps/src/main/java/com/afm/trabalho_ps/service/ItemProdutoService.java
@@ -0,0 +1,51 @@
+package com.afm.trabalho_ps.service;
+
+import com.afm.trabalho_ps.model.ItemProduto;
+import com.afm.trabalho_ps.repository.ItemProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ItemProdutoService {
+
+    @Autowired
+    private ItemProdutoRepository itemProdutoRepository;
+
+    // CRUD: CREATE
+    public void salvar(ItemProduto itemProduto) {
+        itemProdutoRepository.save(itemProduto);
+    }
+
+    // Buscar por ID
+    public Optional<ItemProduto> buscarPorId(Long id) {
+        return itemProdutoRepository.findById(id);
+    }
+
+    // CRUD: READ
+    public List<ItemProduto> listarTodos() {
+        return itemProdutoRepository.findAll();
+    }
+
+    // CRUD: UPDATE
+    public ItemProduto atualizar(Long id, ItemProduto novoItem) {
+        return itemProdutoRepository.findById(id)
+            .map(item -> {
+                item.setProduto(novoItem.getProduto());
+                item.setQuantidade(novoItem.getQuantidade());
+                item.setPreco(novoItem.getPreco());
+                itemProdutoRepository.save(item);
+            })
+            .orElseThrow(() -> new RuntimeException("ItemProduto não encontrado com ID: " + id));
+    }
+
+    // CRUD: DELETE
+    public void deletar(Long id) {
+        if (!itemProdutoRepository.existsById(id)) {
+            throw new RuntimeException("ItemProduto não encontrado com ID: " + id);
+        }
+        itemProdutoRepository.deleteById(id);
+    }
+}
