c56cb5690ba078a1e2180dd166a8c4fcc37800af
Rafael Accetta <rafaelaccetta@id.uff.br>
2025-06-25 11:41:05 -0300

    create GerenciadorNotificacao

---
+++ trabalho-ps/src/main/java/com/afm/trabalho_ps/model/GerenciadorNotificacao.java
@@ -0,0 +1,21 @@
+package com.afm.trabalho_ps.model;
+
+import java.util.List;
+
+public class GerenciadorNotificacao {
+
+    //IMPLEMENTAR SINGLETON
+
+    private List<Usuario> admins;
+
+    public void notificaFaltaDeInsumo(Insumo insumo){
+
+    }
+
+    public void notificaRespostaEncomenda(Usuario cliente, Produto produto, int quantidade, boolean confirmado){
+        
+    }
+
+
+
+}
