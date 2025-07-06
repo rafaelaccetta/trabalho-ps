77ffa4ea22bfc7f1aa336c2ac15a64a5950735ec
Luisa Stellet <luisa.stellet@id.uff.br>
2025-07-06 13:00:40 -0300

    Removendo arquivos não utilizados

--- trabalho-ps/src/main/java/com/afm/trabalho_ps/controller/CatalogoController.java
+++
@@ -1,2 +0,0 @@
-//Responsável pela página web onde o usuário vê o catálogo de produtos disponíveis para compra.
-package com.afm.trabalho_ps.controller;
--- trabalho-ps/src/main/java/com/afm/trabalho_ps/controller/CompraController.java
+++
@@ -1,2 +0,0 @@
-//Responsável pela página web onde o usuário pode gerenciar as compras realizadas por ele.
-package com.afm.trabalho_ps.controller;
